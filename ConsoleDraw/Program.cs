97cfbc0060b705b265542166744dfd0accf85d84
Kirill Malanyin <kmalanyin@appliedtech.ru>
2021-09-11 15:06:05 +0500

    added ConsoleDraw project

---
+++ ConsoleDraw/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleDraw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 50;
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    Console.Write(
+                        Math.Pow(i + j, 2) > Math.Pow(i - j, 4) ? "# " : ". "
+                        //Math.Sqrt(i + j) >= Math.Sqrt(j - i) ? "# " : ". "
+                        );
+
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
