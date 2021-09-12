8d9468c1ba436e30fe17699d6b40fb38c35df4b4
Kirill Malanyin <kmalanyin@appliedtech.ru>
2021-09-12 13:37:24 +0500

    added Draw3DPicture project

---
+++ Draw3DPicture/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Draw3DPicture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawPicture();
+        }
+
+        private void DrawPicture()
+        {
+            int XLength = 400;
+            int YLength = 400;
+            int step = XLength / 10;
+
+            for (int y = 0; y <= YLength; y += step)
+            {
+                DrawLine(0, y, XLength - y, 0);
+                DrawLine(XLength, y, XLength - y, YLength);
+            }
+        }
+
+        private void DrawLine(int x1, int y1, int x2, int y2)
+        {
+            var line = new Line();
+            line.Stroke = Brushes.CadetBlue;
+            line.X1 = x1;
+            line.Y1 = y1;
+            line.X2 = x2;
+            line.Y2 = y2;
+
+            canvas.Children.Add(line);
+        }
+    }
+}
